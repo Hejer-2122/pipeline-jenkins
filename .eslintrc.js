541a8427670a7c4033b279c7b8ec4b3bd32f4a2b
mouauia <benaissamouauia7@gmail.com>
2021-10-18 15:08:35 +0100

    changes1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
-
 module.exports = {
-  'extends': 'standard',
-  'plugins': [
-    'standard',
-    'promise'
-  ],
-  'env': {
-    'node': true
-  },
-  'rules': {
-    "capitalized-comments": [
-      "error",
-      "always"
-    ],
-    "spaced-comment": ["error", "always"]
-  }
-}
+    "extends": "standard"
+};
